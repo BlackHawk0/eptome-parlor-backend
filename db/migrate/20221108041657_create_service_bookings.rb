e8946c125095bf85984c463113153ee5eaedb859
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-11-08 12:27:52 +0300

    changed phone number to string

--- db/migrate/20221108041657_create_service_bookings.rb
+++ db/migrate/20221108041657_create_service_bookings.rb
@@ -2,7 +2,7 @@ class CreateServiceBookings < ActiveRecord::Migration[6.1]
   def change
     create_table :bookings do |t|
       t.string :customer_name
-      t.integer :customer_phonenumber
+      t.string :customer_phonenumber
       t.string :customer_email
       t.datetime :booked_date
       t.integer :service_id
